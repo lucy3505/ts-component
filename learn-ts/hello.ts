865b50956a619103096d8ef45bee03be9f1dd967
lucy3505 <415492767@qq.com>
2022-02-11 22:25:26 +0800

    process

--- learn-ts/hello.ts
+++
@@ -1,6 +0,0 @@
-const hello = (name: string) => {
-  return `hello ${name}`;
-};
-
-console.log(hello("li"));
-// http://ts.xcatliu.com/advanced/declaration-merging.html
--- ts-with-react/src/App.tsx
+++ ts-with-react/src/App.tsx
@@ -1,30 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Hello from "./components/hello";
-import LikeButton2 from "./components/LikeButton2";
-import ContextButton from "./components/ContextButton";
-// import MouseTracker from "./components/MouseTracker";
-import MouseMove from "./components/MouseMove";
-import DogShow2 from "./components/DogShow2";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {/* <MouseTracker /> */}
-        <DogShow2 />
-        <MouseMove />
-        <Hello message="hello2"></Hello>
-        <LikeButton2></LikeButton2>
-        <ContextButton />
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
